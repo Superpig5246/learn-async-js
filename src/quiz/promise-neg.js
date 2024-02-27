f2712986abfd439cc164d8cfa69bc2dda86db0df
Nicholas Soares <nsoares22@lasalle-academy.org>
2024-02-27 17:00:13 -0500

    any and all quizzes completed

--- src/quiz/promise-neg.js
+++ src/quiz/promise-neg.js
@@ -1,3 +1,43 @@
+function sum2DArray(arr) {
+    return new Promise((resolve, reject) => {
+        console.log('Sum called ... ');
+        if(Array.isArray(arr)) {
+            setTimeout(() => {
+                let sum = 0;
+                Promise.any(arr.map((ar) => sum1DArray(ar))).then((response) => {
+                    console.log(response)
+                })
+
+                console.log('resolving ... ');
+                resolve(sum);
+            }, 0);
+        }
+        else {
+            console.log('rejecting ... ');
+            reject('BAD INPUT: Expected array as input');
+        }
+        console.log('returning from sum');
+    });
+}
+
+function sum1DArray(arr) {
+    return new Promise((resolve, reject) => {
+        if (Array.isArray(arr)) {
+            setTimeout(() => {
+                let filtered = arr.filter(num => {
+                    num < 0
+                });
+                if (filtered.length > 0)
+                    resolve(filtered)
+                else
+                    reject("No negative found")
+            },0)
+        } else {
+            reject('BAD INPUT: Expected array as input')
+        }
+    })
+}
+
 const array2D = [
     [1, 2, 3],
     [4, 5, 6],
--- src/quiz/promise-sum-2darray.js
+++ src/quiz/promise-sum-2darray.js
@@ -1,3 +1,41 @@
+function sum2DArray(arr) {
+    return new Promise((resolve, reject) => {
+        console.log('Sum called ... ');
+        if(Array.isArray(arr)) {
+            setTimeout(() => {
+                let sum = 0;
+                Promise.all(arr.map((ar) => sum1DArray(ar))).then((responses) => {
+                    responses.forEach(num => {sum += num})
+                })
+
+                console.log('resolving ... ');
+                resolve(sum);
+            }, 0);
+        }
+        else {
+            console.log('rejecting ... ');
+            reject('BAD INPUT: Expected array as input');
+        }
+        console.log('returning from sum');
+    });
+}
+
+function sum1DArray(arr) {
+    return new Promise((resolve, reject) => {
+        if (Array.isArray(arr)) {
+            setTimeout(() => {
+                let sum = 0; 
+                arr.forEach(num => {
+                    sum += num
+                });
+                resolve(sum)
+            },0)
+        } else {
+            reject('BAD INPUT: Expected array as input')
+        }
+    })
+}
+
 const array2D = [
     [1, 2, 3],
     [4, 5, 6],
